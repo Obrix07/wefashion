6636f2cfdf82e39c0fbeea6cca1efb57687734ad
Obrix07 <leborgnevincent91@gmail.com>
2023-04-30 23:21:46 +0200

    Read.me + change title

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    <title>Document</title>
+    <title>Formulaire</title>
     <style>
         * {
             box-sizing: border-box;
